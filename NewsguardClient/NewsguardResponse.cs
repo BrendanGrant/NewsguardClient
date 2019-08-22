3af9047c20ffa26153116bbf8b89059dfddc7135
Brendan Grant <grantb@dahat.com>
2019-08-21 19:56:51 -0700

    Add project files.

---
+++ NewsguardClient/NewsguardResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsguardClient
+{
+    public class Criterion
+    {
+        public string title { get; set; }
+        public string body { get; set; }
+        public int order { get; set; }
+    }
+
+    public class NewsguardResponse
+    {
+        public string id { get; set; }
+        public string identifier { get; set; }
+        public string rank { get; set; }
+        public double? score { get; set; }
+        public string topline { get; set; }
+        public string country { get; set; }
+        public string language { get; set; }
+        public List<Criterion> criteria { get; set; }
+        public string labelToken { get; set; }
+        public string locale { get; set; }
+    }
+}
---
+++ NewsguardClient/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NewsguardClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var domainName = "newyorktimes.com";
+
+            var c = new HttpClient();
+            var response = await c.GetStringAsync($"https://api.newsguardtech.com/check/{domainName}");
+            var pocoResponse = JsonConvert.DeserializeObject<NewsguardResponse>(response);
+
+            if (pocoResponse.criteria == null)
+            {
+                WriteConsoleMessageWithColor(ConsoleColor.Red, "No criteria found");
+
+                if( pocoResponse.identifier == null)
+                {
+                    WriteConsoleMessageWithColor(ConsoleColor.Yellow, "Unknown (non news) site specified?");
+                }
+                return;
+            }
+
+            Console.WriteLine($"Report for {pocoResponse.identifier} - {pocoResponse.score}%");
+
+            foreach (var item in pocoResponse.criteria)
+            {
+                var color = GetColor(item.body);
+                WriteConsoleMessageWithColor(color, $"{item.title}");
+            }
+        }
+
+        private static ConsoleColor GetColor(string body)
+        {
+            body = body.ToLower();
+
+            if ( body == "yes")
+            {
+                return ConsoleColor.Green;
+            }
+            else if (body == "no")
+            {
+                return ConsoleColor.Red;
+            }
+
+            throw new ArgumentException("unknown input");
+        }
+
+        static void WriteConsoleMessageWithColor(ConsoleColor color, string message)
+        {
+            ConsoleColor backup = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = backup;
+        }
+    }
+}
